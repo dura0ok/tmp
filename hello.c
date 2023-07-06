fc64155d586b9526377016bfc1be30f75f03186c
stepan-neretin7 <fenixrnd@mail.ru>
2023-07-06 17:20:09 +0700

    update

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@
 #include "utils/builtins.h"
 
 char *message = "hello, c world!";
+const char* need = "create table";
+static ProcessUtility_hook_type prev_ProcessUtility = NULL;
+static int counter;
 
 PG_MODULE_MAGIC;
 
@@ -14,11 +17,28 @@ PG_FUNCTION_INFO_V1(hello_cworld);
 Datum
 hello_cworld(PG_FUNCTION_ARGS)
 {
-    PG_RETURN_TEXT_P(cstring_to_text(message));
+    PG_RETURN_INT32(counter);
 }
 
 void
 _PG_init()
-{
+{   
+    counter = 0;
     elog(LOG, "hello world!");
+    prev_ProcessUtility = ProcessUtility_hook;
+    ProcessUtility_hook = pgss_ProcessUtility;
+}
+
+
+static void
+pgss_ProcessUtility(PlannedStmt *pstmt, const char *queryString,
+                    bool readOnlyTree,
+                    ProcessUtilityContext context,
+                    ParamListInfo params, QueryEnvironment *queryEnv,
+                    DestReceiver *dest, QueryCompletion *qc)
+{
+   const char* ptr = strcasestr(str1, "Hello");
+   if(need = ptr){
+      counter++;
+   }
 }
