107c45abb3c578a46c8833d06d25ffe9d596b78f
stepan-neretin7 <fenixrnd@mail.ru>
2023-07-06 18:00:46 +0700

    update

--- hello.c
+++ hello.c
@@ -44,9 +44,11 @@ pgss_ProcessUtility(PlannedStmt *pstmt, const char *queryString,
                     ParamListInfo params, QueryEnvironment *queryEnv,
                     DestReceiver *dest, QueryCompletion *qc)
 {
-   if(prev_ProcessUtility){
-    prev_ProcessUtility();
-   }
+   if (prev_ProcessUtility){
+                prev_ProcessUtility(pstmt, queryString, readOnlyTree,
+                                    context, params, queryEnv,
+                                    dest, qc);
+    }
    const char* ptr = strcasestr(need, "Hello");
    if(need == ptr){
       counter++;
