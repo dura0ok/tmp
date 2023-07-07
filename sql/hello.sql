a20c41addcda17be1d8aeb0934ffcd6153525e8b
stepan-neretin7 <fenixrnd@mail.ru>
2023-07-07 09:53:22 +0700

    add tests

---
+++ sql/hello.sql
@@ -0,0 +1,9 @@
+create extension hello;
+select hello_world();
+select hello_cworld();
+create table kek(t int);
+select hello_cworld();
+create table kek(t int);
+select hello_cworld();
+drop table kek;
+select hello_cworld();
